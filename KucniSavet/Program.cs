b4265773c66fe2c161fdac50d0d2cce718f2ea1e
Tamara Marcetic <tamaramarcetic@hotmail.com>
2019-01-09 09:02:57 +0100

    Changes to be committed: 	modified:   KucniSavet/Program.cs 	new file:   KucniSavet/Zgrada.cs

--- KucniSavet/Program.cs
+++ KucniSavet/Program.cs
@@ -1,19 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KucniSavet
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is my app!");
-            Console.WriteLine("Some changes...");
-
-            Class1.SomeMethod();
+            Console.WriteLine("Unesite informacije o zgradi:");
+            var zgrada = new Zgrada();
+            zgrada.Unos();
         }
     }
 }
---
+++ KucniSavet/Zgrada.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KucniSavet
+{
+    class Zgrada
+    {
+        public string Adresa { get; set; }
+        public short Broj { get; set; }
+        public string Opstina { get; set; }
+        public string Grad { get; set; }
+        public short BrojStanova { get; set; }
+        public List<short> NizStanova { get; set; }
+
+        public void Unos()
+        {
+            Console.Write("Adresa: ");
+            Adresa = Console.ReadLine();
+            Console.Write("Broj: ");
+            Broj = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Opstina: ");
+            Opstina = Console.ReadLine();
+            Console.Write("Grad: ");
+            Grad = Console.ReadLine();
+            Console.Write("Broj stanova: ");
+            BrojStanova = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Niz stanova u kojima zive stanari (uneti brojeve odvojene zarezom):");
+            var tmp = new short[BrojStanova];
+            tmp = Console.ReadLine().Split(',').Select(short.Parse).ToArray();
+            NizStanova = new List<short>();
+            for (int i = 0; i < BrojStanova; i++)
+            {
+                NizStanova.Add(tmp[i]);
+            }
+            
+        }
+
+    }
+}
