7c885c2f51726b4578e271ada82d0ba49d103731
Tamara Marcetic <tamaramarcetic@hotmail.com>
2019-01-09 12:00:20 +0100

    Changes to be committed: 	new file:   KucniSavet/Predsednik.cs 	new file:   KucniSavet/Stanar.cs

---
+++ KucniSavet/Predsednik.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace KucniSavet
+{
+    class Predsednik : Stanar
+    {
+        public override void UnosGodinaRodjenja()
+        {
+            base.UnosGodinaRodjenja();
+            if (DateTime.Now.Year - GodinaRodjenja < 18)
+                throw new ArgumentException("Predsednik mora biti punoletan");
+        }
+
+        public override void UnosNosilac()
+        {
+            base.UnosNosilac();
+            if (!Nosilac)
+                throw new ArgumentException("Predsednik mora biti nosilac stana");
+        }
+
+        public void UnosPredsednik()
+        {
+            UnosIme();
+            UnosPrezime();
+            while (true)
+            {
+                try
+                {
+                    UnosGodinaRodjenja();
+                    break;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            UnosEmail();
+            UnosKorisnickoIme();
+            UnosLozinka();
+            UnosBrojStana();
+            while (true)
+            {
+                try
+                {
+                    UnosNosilac();
+                    break;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ KucniSavet/Stanar.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace KucniSavet
+{
+    class Stanar
+    {
+        protected string Ime;
+        protected string Prezime;
+        protected short GodinaRodjenja;
+        protected string Email;
+        protected string KorisnickoIme;
+        protected string Lozinka;
+        protected short BrojStana;
+        protected bool Nosilac;
+
+        public void UnosIme()
+        {
+            Console.Write("Ime: ");
+            Ime = Console.ReadLine();
+        }
+
+        public void UnosPrezime()
+        {
+            Console.Write("Prezime: ");
+            Prezime = Console.ReadLine();
+        }
+
+        public virtual void UnosGodinaRodjenja()
+        {
+            Console.Write("Godina rodjenja: ");
+            GodinaRodjenja = Convert.ToInt16(Console.ReadLine());
+        }
+
+        public void UnosEmail() { 
+            Console.Write("E-mail adresa: ");
+            Email = Console.ReadLine();
+        }
+
+        public void UnosKorisnickoIme()
+        {
+            Console.Write("Korisnicko ime: ");
+            KorisnickoIme = Console.ReadLine();
+        }
+
+        public void UnosLozinka()
+        {
+            Console.Write("Lozinka: ");
+            Lozinka = Console.ReadLine();
+        }
+
+        public void UnosBrojStana()
+        {
+            Console.Write("Broj stana: ");
+            BrojStana = Convert.ToInt16(Console.ReadLine());
+        }
+
+        public virtual void UnosNosilac()
+        {
+            Console.Write("Da li je nosilac stana (da/ne): ");
+            var tmp = Console.ReadLine();
+            if (tmp.CompareTo("da") == 0)
+                Nosilac = true;
+            else
+                Nosilac = false;
+        }
+
+        public void UnosStanar()
+        {
+            UnosIme();
+            UnosPrezime();
+            UnosGodinaRodjenja();
+            UnosEmail();
+            UnosKorisnickoIme();
+            UnosLozinka();
+            UnosBrojStana();
+            UnosNosilac();
+        }
+    }
+}
